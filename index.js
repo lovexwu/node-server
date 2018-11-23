46d9f473ad0f7e3692341bd342d2a134918e2b5b
lovexwu <1062474646@qq.com>
2018-11-23 18:18:07 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+http.createServer(function(req,res){
+	var pathObj = url.parse(req.url,true)
+	console.log(pathObj)
+
+	switch(pathObj.pathname){
+		case 'getWeather':
+			if(pathObj.query.city == 'beijing'){
+				ret = {
+					city: 'beijing',
+					weather: '晴天'
+				}
+			}else{
+				ret = {
+					city: pathObj.query.city,
+					weather: '不知道'
+				}
+			}
+			res.end(JSON.stringify(ret))
+			break;
+		default:
+			res.end(fs.readFileSync(__dirname + pathObj.pathname))	
+	}
+}).listen(8080)
